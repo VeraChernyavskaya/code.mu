4c75d57714f3d8330d869b2c575bd609f52b8130
VeraChernyavskaya <vchernyavskaya100@gmail.com>
2022-07-20 11:18:12 +0300

    init: start doing exercise from code.mu

---
+++ template strings.js
@@ -0,0 +1,12 @@
+/* Перепишите следующий код через вставку переменных:
+
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt = 'aaa ' + str1 + ' bbb ' + str2 + ' ccc';
+*/
+
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa ${str1} bbb ${str2} ccc`;
+console.log(txt) 
+// "aaa xxx bbb yyy ccc"
