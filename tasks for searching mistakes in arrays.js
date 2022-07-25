3b51d1accb942d7c39e6fac14384c8c01436202f
VeraChernyavskaya <vchernyavskaya100@gmail.com>
2022-07-25 11:28:41 +0300

    feat: add tasks for searching mistakes in arrays

---
+++ tasks for searching mistakes in arrays.js
@@ -0,0 +1,40 @@
+/*
+Код должен вывести последний элемент массива:
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length]);
+*/
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length-1]) // 5
+
+
+/*
+Код должен найти сумму элементов массива:
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[1] + arr[2] + arr[3] + arr[4] + arr[5]);
+*/
+let arr = [1, 2, 3, 4, 5];
+let sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+console.log(sum); // 15
+
+
+/*
+Код должен вывести длину массива:
+
+let arr = {1, 2, 3, 4, 5};
+console.log(arr.length]);
+*/
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.length); // 5
+
+
+/*
+Код должен вывести длину массива:
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.lenght);
+*/
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.length); // 5
