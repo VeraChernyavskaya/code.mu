3e3e3a77d884f9aaecd47e338af4992e97d1607b
VeraChernyavskaya <vchernyavskaya100@gmail.com>
2022-07-26 15:41:51 +0300

    fix: change variables names

--- tasks for searching mistakes in arrays.js
+++ tasks for searching mistakes in arrays.js
@@ -5,8 +5,8 @@ let arr = [1, 2, 3, 4, 5];
 console.log(arr[arr.length]);
 */
 
-let arr = [1, 2, 3, 4, 5];
-console.log(arr[arr.length-1]) // 5
+let array = [1, 2, 3, 4, 5];
+console.log(array[array.length-1]) // 5
 
 
 /*
@@ -15,8 +15,8 @@ console.log(arr[arr.length-1]) // 5
 let arr = [1, 2, 3, 4, 5];
 console.log(arr[1] + arr[2] + arr[3] + arr[4] + arr[5]);
 */
-let arr = [1, 2, 3, 4, 5];
-let sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+let newArr = [1, 2, 3, 4, 5];
+let sum = newArr[0] + newArr[1] + newArr[2] + newArr[3] + newArr[4];
 console.log(sum); // 15
 
 
@@ -26,8 +26,8 @@ console.log(sum); // 15
 let arr = {1, 2, 3, 4, 5};
 console.log(arr.length]);
 */
-let arr = [1, 2, 3, 4, 5];
-console.log(arr.length); // 5
+let arrayNew = [1, 2, 3, 4, 5];
+console.log(arrayNew.length); // 5
 
 
 /*
